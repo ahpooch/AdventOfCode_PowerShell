0c62cca62e4b48e039f3c1a09c4713c2fd2041cd
ahpooch <81159041+ahpooch@users.noreply.github.com>
2025-08-02 13:47:55 +0300

    Added soulution for 2024 Day1

---
+++ 2024/Day1/Day1_Solution.ps1
@@ -0,0 +1,34 @@
+# https://adventofcode.com/2024/day/1
+
+function Get-Answer {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $InputData
+    )
+    
+    $ValuePairs = $InputData.Split("`r`n")
+    
+    $Lefts = @()
+    $Rights = @()
+    
+    foreach ($ValuePair in $ValuePairs) {
+        $Lefts += $ValuePair.Split("   ")[0]
+        $Rights += $ValuePair.Split("   ")[1]
+    }
+    
+    $Lefts = $Lefts | Sort-Object
+    $Rights = $Rights | Sort-Object
+    
+    $Summ = 0
+    for ($i = 0; $i -lt $Lefts.Count; $i++) {
+        $Summ += [Math]::Abs($Lefts[$i] - $Rights[$i])
+    }
+    
+    return $Summ
+}
+
+$Inputs = Get-Content -Path ./Day1_Inputs.txt -Raw
+
+Get-Answer -InputData $Inputs
