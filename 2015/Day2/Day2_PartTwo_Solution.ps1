aaf4d31bc111b6120f5626f1dae3bd457fc88258
ahpooch <81159041+ahpooch@users.noreply.github.com>
2025-08-04 19:51:15 +0300

    Added solution for 2015 Day2 Part Two

---
+++ 2015/Day2/Day2_PartTwo_Solution.ps1
@@ -0,0 +1,44 @@
+# https://adventofcode.com/2015/day/2
+
+function Get-Answer {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $InputData
+    )
+
+    class Box {
+        [int] $Length
+        [int] $Width
+        [int] $Height
+        [int] $SmallestPerimeter
+        [int] $BowLength
+
+        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = '$sp is used in different scriptblocks of ForEach')]
+        Box([string] $Dimentions) {
+            $this.Length = $Dimentions.split("x")[0]
+            $this.Width = $Dimentions.split("x")[1]
+            $this.Height = $Dimentions.split("x")[2]
+            $this.SmallestPerimeter = $this.Length, $this.Width, $this.Height | `
+                Sort-Object | Select-Object -First 2 | `
+                ForEach-Object -Begin { $sp = 0 } -Process { $sp += 2 * $_ } -End { $sp }
+            $this.BowLength = $this.Length * $this.Width * $this.Height
+        }
+
+        [int] RequiredRibbonLength() {
+            return $this.SmallestPerimeter + $this.BowLength
+        }
+    }
+
+    $Boxes = @()
+    foreach ($Data in $InputData.Split("`r`n")) {
+        $Boxes += New-Object Box $Data
+    }
+
+    return ($Boxes.RequiredRibbonLength() | Measure-Object -Sum).Sum
+}
+
+$Inputs = Get-Content -Path "$PSScriptRoot\Day2_Inputs.txt" -Raw
+
+Get-Answer -InputData $Inputs
